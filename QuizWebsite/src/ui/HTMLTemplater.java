18d8b8e14f20fb3bcbe24be993ffae3ebc097b4d
Joseph Delgado <delgadoj@Josephs-MacBook-Pro.local>
2013-11-24 02:02:19 -0800

    Added Admin Panel

--- QuizWebsite/src/ui/HTMLTemplater.java
+++ QuizWebsite/src/ui/HTMLTemplater.java
@@ -25,7 +25,7 @@ public class HTMLTemplater {
 		return 
 		"<div id=\"blue-bar\">"+
 			"<div id=\"page-head\" class=\"page-width wide\">"+
-				"<a href=\"/\" id=\"masthead-logo\" class=\"sprite logo\"></a>"+
+				"<a href=\"/QuizWebsite/\" id=\"masthead-logo\" class=\"sprite logo\"></a>"+
 				"<div class=\"header-wrapper\">"+
 					"<div class=\"navbar right\">"+
 						"<ul class=\"nav\">"+
@@ -33,10 +33,12 @@ public class HTMLTemplater {
 							"<li class=\"sprite messages\"></li>"+
 							"<li class=\"sprite notifications\"></li>"+
 						"</ul>"+
-						"<a id=\"username-link-header\" href=\"user_url\">"+username+"</a>"+
+						"<a class=\"header-text\" href=\""+user_url+"\">"+username+"</a>"+
+						"<a class=\"header-text\" href=\"/QuizWebsite/\">Home</a>"+
+						"<a class=\"sprite2 admin admin-button\" href=\"/QuizWebsite/Admin.jsp\"></a>"+
 					"</div>"+
 					"<div class=\"search-bar\">"+
-						"<input type=\"text\" id=\"graph-search-bar\">"+
+						"<input type=\"text\" id=\"graph-search-bar\" placeholder=\"Search for People, Quizzes, and More\">"+
 						"<div class=\"sprite search-mag\" id=\"search-mag\"></div>"+
 					"</div>"+
 				"</div>"+
