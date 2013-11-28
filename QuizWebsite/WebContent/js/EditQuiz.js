6edffa4fc2ef5aab8d66786b7d2ece4e2839560a
Joseph Delgado <delgadoj@Josephs-MacBook-Pro.local>
2013-11-27 20:05:32 -0800

    add new line implemented

--- QuizWebsite/WebContent/js/EditQuiz.js
+++ QuizWebsite/WebContent/js/EditQuiz.js
@@ -1,6 +1,7 @@
 /**
  * 
  */
+ var num = 0;
  function init_js (quiz_id) {
  	new QuestionsHandler(document.getElementById('questions-ul'), quiz_id);
  }
@@ -18,26 +19,33 @@
 
  	function get_divider () {
  		var li = document.createElement('li');
- 		//li.style.borderBottom = '1px solid rgba(0,0,0,0.3)';
  		var line_left  = document.createElement('span');
  		var line_right = document.createElement('span');
- 		line_left.style.width = '48%';
- 		line_right.style.width = '48%';
- 		line_left.style.borderBottom = '1px solid rgba(0,0,0,0.3)';
- 		line_right.style.borderBottom = '1px solid rgba(0,0,0,0.3)';
- 		line_left.style.float = 'left';
- 		line_right.style.float = 'right';
- 		li.style.position = 'relative';
+ 		line_left.classList.add('line','left');
+ 		line_right.classList.add('line','right');
  		li.appendChild(line_left);
  		li.appendChild(line_right);
 
- 		var add = document.createElement('span');
- 		add.style.width = '4%';
- 		add.style.height = add.style.width;
- 		add.innerHTML = '+';
- 		add.style.border = '1px solid rgba(0,0,0,0.3)';
- 		add.style.borderRadius = '50%';
- 		li.appendChild(add);
+ 		var plus_circle = document.createElement('div');
+ 		var plus        = document.createElement('span')
+ 		plus_circle.addEventListener('click', function () {
+ 			var parent = li.parentElement;
+ 			parent.insertBefore(get_divider(), li);
+ 			parent.insertBefore(new_elem(), li);
+ 		});
+
+ 		plus_circle.classList.add('plus-circle')
+ 		plus.classList.add('plus');
+ 		plus.innerHTML = '+';
+ 		plus_circle.appendChild(plus);
+ 		li.appendChild(plus_circle);
+ 		return li;
+ 	}
+
+ 	function new_elem () {
+ 		var li = document.createElement('li');
+ 		li.classList.add('question-section');
+ 		li.innerHTML = 'hi'+ (num++);
  		return li;
  	}
  }
