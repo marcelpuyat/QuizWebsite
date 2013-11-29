8c9cb60a25a0131ad1c6cf163229b14b15be781b
Joseph Delgado <delgadoj@Josephs-MacBook-Pro.local>
2013-11-29 00:27:14 -0800

    moving towards spec

--- QuizWebsite/WebContent/js/EditQuizTypes.js
+++ QuizWebsite/WebContent/js/EditQuizTypes.js
@@ -76,12 +76,13 @@ function TypeHandler (wrapper, parent, quiz_id) {
 			if (q_handler) {
 				var q_reap = q_handler.reap();
 				questions.push(q_reap);//add score to max score
-				total_score+=parseInt(q_reap.score);
+				total_score+=parseInt(q_reap.data.score);
 			}
 		};
 		var jsonResponse = _parent.getMeta();
 		jsonResponse.questions = questions;
 		jsonResponse.max_score = total_score;
+		jsonResponse.quiz_id = parseInt(_quiz_id);
 		console.log('sending to :'+'/QuizWebsite/QuizServlet?quiz_id='+_quiz_id);
 		console.log(jsonResponse);
 		post_json_to_url(
@@ -101,7 +102,7 @@ function MultipleChoiceHandler (parent, type) {
 	var _data;
 	var _parent = parent;
 	var _prompt;
-	var _options_ul;
+	var _answers_ul;
 	var _score_input;
 	this.getElem = function () {
 		var ul = document.createElement('ul');
@@ -157,28 +158,28 @@ function MultipleChoiceHandler (parent, type) {
 		prompt_li.appendChild(_prompt);
 		ul.appendChild(prompt_li);
 
-		/* options title */
-		var options_title_li = document.createElement('li');
-		var options_title = document.createElement('div');
-		options_title.classList.add('faint');
-		options_title.innerHTML = 'options';
-		options_title_li.appendChild(options_title);
-		ul.appendChild(options_title_li);
+		/* answers title */
+		var answers_title_li = document.createElement('li');
+		var answers_title = document.createElement('div');
+		answers_title.classList.add('faint');
+		answers_title.innerHTML = 'answers';
+		answers_title_li.appendChild(answers_title);
+		ul.appendChild(answers_title_li);
 
-		/* options */
-		var options_container_li = document.createElement('li');
-		_options_ul = document.createElement('ul');
+		/* answers */
+		var answers_container_li = document.createElement('li');
+		_answers_ul = document.createElement('ul');
 		if (_data && _data.data && _data.data.options) {
-			var options = _data.data.options;
-			for (var i = 0; i < options.length; i++) {
+			var answers = _data.data.options;
+			for (var i = 0; i < answers.length; i++) {
 				var checked = (i == _data.data.correct);
-				_options_ul.appendChild(get_choice(options[i],checked, false));
+				_answers_ul.appendChild(get_choice(answers[i],checked, false));
 			};
 		}
-		append_blank_option(_options_ul);
-		options_container_li.appendChild(_options_ul);
+		append_blank_option(_answers_ul);
+		answers_container_li.appendChild(_answers_ul);
 
-		ul.appendChild(options_container_li);
+		ul.appendChild(answers_container_li);
 
 		return ul;
 	}
@@ -186,13 +187,15 @@ function MultipleChoiceHandler (parent, type) {
 		_data = data;
 	};
 	this.reap = function () {
-		var opsObj = get_options();
+		var opsObj = get_answers();
 		return {
 			type:_type.json_name,
-			prompt:_prompt.value,
-			options:opsObj.options,
-			correct:opsObj.checked,
-			score:parseInt(_score_input.value)
+			data:{
+				prompt:_prompt.value,
+				options:opsObj.answers,
+				correct:opsObj.checked,
+				score:parseInt(_score_input.value)
+			}
 		};
 	};
 	function append_blank_option (ul) {
@@ -205,21 +208,20 @@ function MultipleChoiceHandler (parent, type) {
 		this.removeEventListener('keyup',new_option_keyup_handler);
 		append_blank_option(this.ul);
 	}
-	function get_options () {
-		var options_lis = document.getElementsByClassName('multiple-choice-li-'+_id);
+	function get_answers () {
+		var answers_lis = document.getElementsByClassName('multiple-choice-li-'+_id);
 		var checked = -1;
-		var options = [];
-		for (var i = 0; i < options_lis.length; i++) {
-			var children = options_lis[i].children;
-			if (options_lis[i].text.value != "") {
-				options.push(options_lis[i].text.value);
-				if (options_lis[i].radio.checked) {
-					checked = options.length - 1; //will be last element in array
+		var answers = [];
+		for (var i = 0; i < answers_lis.length; i++) {
+			var children = answers_lis[i].children;
+			if (answers_lis[i].text.value != "") {
+				answers.push(answers_lis[i].text.value);
+				if (answers_lis[i].radio.checked) {
+					checked = answers.length - 1; //will be last element in array
 				}
 			}
 		};
-		console.log('checked: '+options[checked]);
-		return {options:options, checked:checked};
+		return {answers:answers, checked:checked};
 	}
 	function get_choice (value, checked, disabled) {
 		console.log(value);
@@ -262,7 +264,7 @@ function MultipleAnswerHandler (parent, type) {
 	var _data;
 	var _parent = parent;
 	var _prompt;
-	var _options_ul;
+	var _answers_ul;
 	var _score_input;
 	this.getElem = function () {
 		var ul = document.createElement('ul');
@@ -318,29 +320,29 @@ function MultipleAnswerHandler (parent, type) {
 		prompt_li.appendChild(_prompt);
 		ul.appendChild(prompt_li);
 
-		/* options title */
-		var options_title_li = document.createElement('li');
-		var options_title = document.createElement('div');
-		options_title.classList.add('faint');
-		options_title.innerHTML = 'options';
-		options_title_li.appendChild(options_title);
-		ul.appendChild(options_title_li);
+		/* answers title */
+		var answers_title_li = document.createElement('li');
+		var answers_title = document.createElement('div');
+		answers_title.classList.add('faint');
+		answers_title.innerHTML = 'answers';
+		answers_title_li.appendChild(answers_title);
+		ul.appendChild(answers_title_li);
 
-		/* options */
-		var options_container_li = document.createElement('li');
-		_options_ul = document.createElement('ul');
-		if (_data && _data.data && _data.data.answers) {
-			var answers = _data.data.answers;
+		/* answers */
+		var answers_container_li = document.createElement('li');
+		_answers_ul = document.createElement('ul');
+		if (_data && _data.data && _data.data.correct) {
+			var answers = _data.data.correct;
 			for (var i = 0; i < answers.length; i++) {
 				var checked = (answers[i][1]);
 				var text    = (answers[i][0]);
-				_options_ul.appendChild(get_choice(text,checked, false));
+				_answers_ul.appendChild(get_choice(text,checked, false));
 			};
 		}
-		append_blank_option(_options_ul);
-		options_container_li.appendChild(_options_ul);
+		append_blank_option(_answers_ul);
+		answers_container_li.appendChild(_answers_ul);
 
-		ul.appendChild(options_container_li);
+		ul.appendChild(answers_container_li);
 
 		return ul;
 	}
@@ -351,9 +353,12 @@ function MultipleAnswerHandler (parent, type) {
 		var ansObj = get_answers();
 		return {
 			type:_type.json_name,
-			prompt:_prompt.value,
-			answers:ansObj.answers,
-			score:parseInt(_score_input.value)
+			data:{
+				prompt:_prompt.value,
+				partial_credit:true,
+				correct:ansObj.answers,
+				score:parseInt(_score_input.value)
+			}
 		};
 	};
 	function append_blank_option (ul) {
@@ -367,13 +372,13 @@ function MultipleAnswerHandler (parent, type) {
 		append_blank_option(this.ul);
 	}
 	function get_answers () {
-		var options_lis = document.getElementsByClassName('multiple-choice-li-'+_id);
+		var answers_lis = document.getElementsByClassName('multiple-choice-li-'+_id);
 		var checked = -1;
 		var answers = [];
-		for (var i = 0; i < options_lis.length; i++) {
-			var children = options_lis[i].children;
-			if (options_lis[i].text.value != "") {
-				answers.push([options_lis[i].text.value,options_lis[i].check_box.checked]);
+		for (var i = 0; i < answers_lis.length; i++) {
+			var children = answers_lis[i].children;
+			if (answers_lis[i].text.value != "") {
+				answers.push([answers_lis[i].text.value,answers_lis[i].check_box.checked]);
 			}
 		};
 		return {answers:answers};
@@ -420,7 +425,7 @@ function PictureResponseHandler (parent, type) {
 	var _parent = parent;
 	var _prompt;
 	var _url;
-	var _options_ul;
+	var _answers_ul;
 	var _score_input;
 	this.getElem = function () {
 		var ul = document.createElement('ul');
@@ -496,28 +501,28 @@ function PictureResponseHandler (parent, type) {
 		prompt_li.appendChild(_prompt);
 		ul.appendChild(prompt_li);
 
-		/* options title */
-		var options_title_li = document.createElement('li');
-		var options_title = document.createElement('div');
-		options_title.classList.add('faint');
-		options_title.innerHTML = 'options';
-		options_title_li.appendChild(options_title);
-		ul.appendChild(options_title_li);
+		/* answers title */
+		var answers_title_li = document.createElement('li');
+		var answers_title = document.createElement('div');
+		answers_title.classList.add('faint');
+		answers_title.innerHTML = 'answers';
+		answers_title_li.appendChild(answers_title);
+		ul.appendChild(answers_title_li);
 
-		/* options */
-		var options_container_li = document.createElement('li');
-		_options_ul = document.createElement('ul');
+		/* answers */
+		var answers_container_li = document.createElement('li');
+		_answers_ul = document.createElement('ul');
 		if (_data && _data.data && _data.data.correct) {
 			var correct = _data.data.correct;
 			for (var i = 0; i < correct.length; i++) {
 				var text    = (correct[i]);
-				_options_ul.appendChild(get_choice(text));
+				_answers_ul.appendChild(get_choice(text));
 			};
 		}
-		append_blank_option(_options_ul);
-		options_container_li.appendChild(_options_ul);
+		append_blank_option(_answers_ul);
+		answers_container_li.appendChild(_answers_ul);
 
-		ul.appendChild(options_container_li);
+		ul.appendChild(answers_container_li);
 
 		return ul;
 	}
@@ -528,10 +533,12 @@ function PictureResponseHandler (parent, type) {
 		var ansObj = get_correct();
 		return {
 			type:_type.json_name,
-			prompt:_prompt.value,
-			img_url:_url.value,
-			correct:ansObj.correct,
-			score:parseInt(_score_input.value)
+			data: {
+				img_url:_url.value,
+				prompt:_prompt.value,
+				correct:ansObj.correct,
+				score:parseInt(_score_input.value)
+			}
 		};
 	};
 	function append_blank_option (ul) {
@@ -545,12 +552,12 @@ function PictureResponseHandler (parent, type) {
 		append_blank_option(this.ul);
 	}
 	function get_correct () {
-		var options_lis = document.getElementsByClassName('multiple-choice-li-'+_id);
+		var answers_lis = document.getElementsByClassName('multiple-choice-li-'+_id);
 		var correct = [];
-		for (var i = 0; i < options_lis.length; i++) {
-			var children = options_lis[i].children;
-			if (options_lis[i].text.value != "") {
-				correct.push(options_lis[i].text.value);
+		for (var i = 0; i < answers_lis.length; i++) {
+			var children = answers_lis[i].children;
+			if (answers_lis[i].text.value != "") {
+				correct.push(answers_lis[i].text.value);
 			}
 		};
 		return {correct:correct};
@@ -580,7 +587,7 @@ function MatchingHandler (parent, type) {
 	var _data;
 	var _parent = parent;
 	var _prompt;
-	var _options_ul;
+	var _answers_ul;
 	var _score_input;
 	this.getElem = function () {
 		var ul = document.createElement('ul');
@@ -636,29 +643,29 @@ function MatchingHandler (parent, type) {
 		prompt_li.appendChild(_prompt);
 		ul.appendChild(prompt_li);
 
-		/* options title */
-		var options_title_li = document.createElement('li');
-		var options_title = document.createElement('div');
-		options_title.classList.add('faint');
-		options_title.innerHTML = 'options';
-		options_title_li.appendChild(options_title);
-		ul.appendChild(options_title_li);
+		/* answers title */
+		var answers_title_li = document.createElement('li');
+		var answers_title = document.createElement('div');
+		answers_title.classList.add('faint');
+		answers_title.innerHTML = 'answers';
+		answers_title_li.appendChild(answers_title);
+		ul.appendChild(answers_title_li);
 
-		/* options */
-		var options_container_li = document.createElement('li');
-		_options_ul = document.createElement('ul');
-		if (_data && _data.data && _data.data.answers) {
-			var answers = _data.data.answers;
+		/* answers */
+		var answers_container_li = document.createElement('li');
+		_answers_ul = document.createElement('ul');
+		if (_data && _data.data && _data.data.correct) {
+			var answers = _data.data.correct;
 			for (var i = 0; i < answers.length; i++) {
 				var left = (answers[i][0]);
 				var right    = (answers[i][1]);
-				_options_ul.appendChild(get_choice(left,right));
+				_answers_ul.appendChild(get_choice(left,right));
 			};
 		}
-		append_blank_option(_options_ul);
-		options_container_li.appendChild(_options_ul);
+		append_blank_option(_answers_ul);
+		answers_container_li.appendChild(_answers_ul);
 
-		ul.appendChild(options_container_li);
+		ul.appendChild(answers_container_li);
 
 		return ul;
 	}
@@ -669,9 +676,11 @@ function MatchingHandler (parent, type) {
 		var ansObj = get_answers();
 		return {
 			type:_type.json_name,
-			prompt:_prompt.value,
-			answers:ansObj.answers,
-			score:parseInt(_score_input.value)
+			data: {
+				prompt:_prompt.value,
+				correct:ansObj.answers,
+				score:parseInt(_score_input.value)
+			}
 		};
 	};
 	function append_blank_option (ul) {
@@ -685,13 +694,13 @@ function MatchingHandler (parent, type) {
 		append_blank_option(this.ul);
 	}
 	function get_answers () {
-		var options_lis = document.getElementsByClassName('multiple-choice-li-'+_id);
+		var answers_lis = document.getElementsByClassName('multiple-choice-li-'+_id);
 		var checked = -1;
 		var answers = [];
-		for (var i = 0; i < options_lis.length; i++) {
-			var children = options_lis[i].children;
-			if (options_lis[i].left_text.value != "" && options_lis[i].right_text.value != "") {
-				answers.push([options_lis[i].left_text.value,options_lis[i].right_text.value]);
+		for (var i = 0; i < answers_lis.length; i++) {
+			var children = answers_lis[i].children;
+			if (answers_lis[i].left_text.value != "" && answers_lis[i].right_text.value != "") {
+				answers.push([answers_lis[i].left_text.value,answers_lis[i].right_text.value]);
 			}
 		};
 		return {answers:answers};
@@ -730,7 +739,7 @@ function FillInBlankHandler (parent, type) {
 	var _prompt;
 	var _before_prompt
 	var _after_prompt;
-	var _options_ul;
+	var _answers_ul;
 	var _score_input;
 	this.getElem = function () {
 		var ul = document.createElement('ul');
@@ -808,28 +817,28 @@ function FillInBlankHandler (parent, type) {
 		ul.appendChild(before_prompt_li);
 
 
-		/* options title */
-		var options_title_li = document.createElement('li');
-		var options_title = document.createElement('div');
-		options_title.classList.add('faint');
-		options_title.innerHTML = 'options';
-		options_title_li.appendChild(options_title);
-		ul.appendChild(options_title_li);
+		/* answers title */
+		var answers_title_li = document.createElement('li');
+		var answers_title = document.createElement('div');
+		answers_title.classList.add('faint');
+		answers_title.innerHTML = 'answers';
+		answers_title_li.appendChild(answers_title);
+		ul.appendChild(answers_title_li);
 
-		/* options */
-		var options_container_li = document.createElement('li');
-		_options_ul = document.createElement('ul');
-		if (_data && _data.data && _data.data.answers) {
-			var answers = _data.data.answers;
+		/* answers */
+		var answers_container_li = document.createElement('li');
+		_answers_ul = document.createElement('ul');
+		if (_data && _data.data && _data.data.correct) {
+			var answers = _data.data.correct;
 			if (answers.length == undefined) answers = [answers];
 			for (var i = 0; i < answers.length; i++) {
 				var text    = (answers[i]);
-				_options_ul.appendChild(get_choice(text));
+				_answers_ul.appendChild(get_choice(text));
 			};
 		}
-		append_blank_option(_options_ul);
-		options_container_li.appendChild(_options_ul);
-		ul.appendChild(options_container_li);
+		append_blank_option(_answers_ul);
+		answers_container_li.appendChild(_answers_ul);
+		ul.appendChild(answers_container_li);
 
 
 		/* after prompt title */
@@ -861,11 +870,13 @@ function FillInBlankHandler (parent, type) {
 		var ansObj = get_correct();
 		return {
 			type:_type.json_name,
-			first_prompt:_before_prompt.value,
-			second_prompt:_after_prompt.value,
-			optional_prompt:_prompt.value,
-			correct:ansObj.correct,
-			score:parseInt(_score_input.value)
+			data:{
+				optional_prompt:_prompt.value,
+				first_prompt:_before_prompt.value,
+				second_prompt:_after_prompt.value,
+				correct:ansObj.correct,
+				score:parseInt(_score_input.value)
+			}
 		};
 	};
 	function append_blank_option (ul) {
@@ -879,12 +890,12 @@ function FillInBlankHandler (parent, type) {
 		append_blank_option(this.ul);
 	}
 	function get_correct () {
-		var options_lis = document.getElementsByClassName('multiple-choice-li-'+_id);
+		var answers_lis = document.getElementsByClassName('multiple-choice-li-'+_id);
 		var correct = [];
-		for (var i = 0; i < options_lis.length; i++) {
-			var children = options_lis[i].children;
-			if (options_lis[i].text.value != "") {
-				correct.push(options_lis[i].text.value);
+		for (var i = 0; i < answers_lis.length; i++) {
+			var children = answers_lis[i].children;
+			if (answers_lis[i].text.value != "") {
+				correct.push(answers_lis[i].text.value);
 			}
 		};
 		return {correct:correct};
@@ -915,7 +926,7 @@ function SingleAnswerHandler (parent, type) {
 	var _data;
 	var _parent = parent;
 	var _prompt;
-	var _options_ul;
+	var _answers_ul;
 	var _score_input;
 	this.getElem = function () {
 		var ul = document.createElement('ul');
@@ -971,28 +982,28 @@ function SingleAnswerHandler (parent, type) {
 		prompt_li.appendChild(_prompt);
 		ul.appendChild(prompt_li);
 
-		/* options title */
-		var options_title_li = document.createElement('li');
-		var options_title = document.createElement('div');
-		options_title.classList.add('faint');
-		options_title.innerHTML = 'options';
-		options_title_li.appendChild(options_title);
-		ul.appendChild(options_title_li);
+		/* answers title */
+		var answers_title_li = document.createElement('li');
+		var answers_title = document.createElement('div');
+		answers_title.classList.add('faint');
+		answers_title.innerHTML = 'answers';
+		answers_title_li.appendChild(answers_title);
+		ul.appendChild(answers_title_li);
 
-		/* options */
-		var options_container_li = document.createElement('li');
-		_options_ul = document.createElement('ul');
+		/* answers */
+		var answers_container_li = document.createElement('li');
+		_answers_ul = document.createElement('ul');
 		if (_data && _data.data && _data.data.correct) {
 			var correct = _data.data.correct;
 			for (var i = 0; i < correct.length; i++) {
 				var text    = (correct[i]);
-				_options_ul.appendChild(get_choice(text));
+				_answers_ul.appendChild(get_choice(text));
 			};
 		}
-		append_blank_option(_options_ul);
-		options_container_li.appendChild(_options_ul);
+		append_blank_option(_answers_ul);
+		answers_container_li.appendChild(_answers_ul);
 
-		ul.appendChild(options_container_li);
+		ul.appendChild(answers_container_li);
 
 		return ul;
 	}
@@ -1019,12 +1030,12 @@ function SingleAnswerHandler (parent, type) {
 		append_blank_option(this.ul);
 	}
 	function get_correct () {
-		var options_lis = document.getElementsByClassName('multiple-choice-li-'+_id);
+		var answers_lis = document.getElementsByClassName('multiple-choice-li-'+_id);
 		var correct = [];
-		for (var i = 0; i < options_lis.length; i++) {
-			var children = options_lis[i].children;
-			if (options_lis[i].text.value != "") {
-				correct.push(options_lis[i].text.value);
+		for (var i = 0; i < answers_lis.length; i++) {
+			var children = answers_lis[i].children;
+			if (answers_lis[i].text.value != "") {
+				correct.push(answers_lis[i].text.value);
 			}
 		};
 		return {correct:correct};
