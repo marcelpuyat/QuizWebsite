00434fdbed6e2ca73e8a6c6d5cee5055bd3ec1cc
Joseph Delgado <delgadoj@Josephs-MacBook-Pro.local>
2013-11-30 00:45:45 -0800

    Statements removed from Graph

--- QuizWebsite/src/graph/GraphSearch.java
+++ QuizWebsite/src/graph/GraphSearch.java
@@ -3,7 +3,6 @@ package graph;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.HashSet;
 import java.util.Set;
 
