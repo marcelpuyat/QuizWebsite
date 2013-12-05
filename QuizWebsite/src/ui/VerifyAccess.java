88fdb42229fe7040b91985d87f71723317cfb9e9
Reid Watson <rawatson@stanford.edu>
2013-12-04 20:57:47 -0800

    Added a success/fail return value to VerifyAccess.verify

--- QuizWebsite/src/ui/VerifyAccess.java
+++ QuizWebsite/src/ui/VerifyAccess.java
@@ -11,21 +11,23 @@ import user.*;
 
 public class VerifyAccess {
 
-	public static void verify(String servletName, HttpSession session, HttpServletRequest request, HttpServletResponse response) {
+	public static boolean verify(String servletName, HttpSession session, HttpServletRequest request, HttpServletResponse response) {
 		try {
 			User u = (User)session.getAttribute("user");
-			if (servletName.equals("Login.jsp")) return;
+			if (servletName.equals("Login.jsp")) return true;
 			if (u == null) {
 				response.sendRedirect("/QuizWebsite/Login.jsp");
-				return;
+				return false;
 			}
-			if (servletName.equals("Settings.jsp")) return;
+			if (servletName.equals("Settings.jsp")) return true;
 			if (!u.hasCompletedProfile()) {
 				response.sendRedirect("/QuizWebsite/Settings.jsp");
-				return;
+				return false;
 			}
+			return true;
 		} catch (IOException e) {
 			e.printStackTrace();
+			return false;
 		}
 	}
 	
